ec191c86b807e6f362745f68ed4e2385037fed98
Andrew Sun <0xandrewsun@gmail.com>
2017-08-04 22:15:41 -0500

    Block "tag ribbons" (the rainbow tag carousel thing), fix the way titles are blocked

--- src/com/crossbowffs/tumblradaway/Hook.java
+++ src/com/crossbowffs/tumblradaway/Hook.java
@@ -9,72 +9,80 @@ import de.robv.android.xposed.callbacks.XC_LoadPackage;
 import java.util.List;
 
 public class Hook implements IXposedHookLoadPackage {
-    private static void removeAds(List<?> timeline) {
-        Xlog.i("Dashboard refresh completed, filtering ads...");
-        int adCount = 0;
-        int postCount = timeline.size();
-        for (int i = postCount - 1; i >= 0; i--) {
-            Object timelineObject = timeline.get(i);
-            if (isAd(timelineObject)) {
-                timeline.remove(i);
-                adCount++;
-            }
-        }
-        Xlog.i("%d/%d posts filtered", adCount, postCount);
-    }
-
-    private static boolean isAd(Object timelineObject) {
+    private static String getTimelineObjectType(Object timelineObject) {
         Object objectData = XposedHelpers.callMethod(timelineObject, "getObjectData");
-        Object postId = XposedHelpers.callMethod(objectData, "getId");
         Enum<?> typeEnum = (Enum<?>)XposedHelpers.callMethod(objectData, "getTimelineObjectType");
-        String typeStr = typeEnum.name();
-
-        if (isAdType(typeStr)) {
-            Xlog.i("Blocked post: getTimelineObjectType() == %s <ID %s>", typeStr, postId);
-            return true;
-        }
-
-        boolean isSponsored = (Boolean)XposedHelpers.callMethod(timelineObject, "isSponsored");
-        if (isSponsored) {
-            Xlog.i("Blocked post: isSponsored() == true <ID %s>", postId);
-            return true;
-        }
-
-        return false;
+        return typeEnum.name();
     }
 
     private static boolean isAdType(String typeStr) {
+        // This list should be ordered in decreasing frequency
+        // of appearance for maximum performance!
         if ("POST".equals(typeStr)) return false;
         if ("BLOG_CARD".equals(typeStr)) return false;
+        if ("TITLE".equals(typeStr)) return false;
+        if ("CHICLET_ROW".equals(typeStr)) return false;
+        if ("TRENDING_TOPIC".equals(typeStr)) return false;
+        if ("FOLLOWED_SEARCH_TAG_RIBBON".equals(typeStr)) return false;
         if ("BANNER".equals(typeStr)) return true;
         if ("CAROUSEL".equals(typeStr)) return true;
         if ("RICH_BANNER".equals(typeStr)) return true;
         if ("GEMINI_AD".equals(typeStr)) return true;
         if ("CLIENT_SIDE_MEDIATION".equals(typeStr)) return true;
         if ("FAN_MEDIATION".equals(typeStr)) return true;
-        if ("TITLE".equals(typeStr)) return true;
+        if ("TAG_RIBBON".equals(typeStr)) return true;
         Xlog.w("Unknown post type: %s", typeStr);
         return false;
     }
 
-    private static void forceEnableGraywater(XC_LoadPackage.LoadPackageParam lpparam) {
-        Xutil.findAndHookMethod(
-            "com.tumblr.App", lpparam.classLoader,
-            "isBeta",
-            XC_MethodReplacement.returnConstant(true));
+    private static boolean isSponsored(Object timelineObject) {
+        return (Boolean)XposedHelpers.callMethod(timelineObject, "isSponsored");
+    }
 
-        Xutil.findAndHookMethod(
-            "com.tumblr.feature.Feature", lpparam.classLoader,
-            "isEnabled", "com.tumblr.feature.Feature",
-            new XC_MethodHook() {
-                @Override
-                protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-                    Enum<?> arg = (Enum<?>)param.args[0];
-                    if ("GRAYWATER_DASHBOARD".equals(arg.name())) {
-                        param.setResult(true);
-                    }
-                }
-            });
+    private static boolean isTitleType(Object typeStr) {
+        return "TITLE".equals(typeStr);
+    }
+
+    private static boolean isAd(Object timelineObject, boolean removedNext) {
+        String objType = getTimelineObjectType(timelineObject);
+
+        if (isAdType(objType)) {
+            Xlog.i("Blocked object: getTimelineObjectType() == %s", objType);
+            return true;
+        }
+
+        if (isSponsored(timelineObject)) {
+            Xlog.i("Blocked object: isSponsored() == true");
+            return true;
+        }
+
+        // Removes any titles that immediately precede an ad
+        // object. Note that not all titles are evil, so we
+        // can't just block them all.
+        if (removedNext && isTitleType(objType)) {
+            Xlog.i("Blocked object: title preceding ad");
+            return true;
+        }
+
+        return false;
+    }
+
+    private static void removeAds(List<?> timeline) {
+        Xlog.i("Dashboard refresh completed, filtering ads...");
+        int adCount = 0;
+        int postCount = timeline.size();
+        boolean removedNext = false;
+        for (int i = postCount - 1; i >= 0; i--) {
+            Object timelineObject = timeline.get(i);
+            if (isAd(timelineObject, removedNext)) {
+                timeline.remove(i);
+                adCount++;
+                removedNext = true;
+            } else {
+                removedNext = false;
+            }
+        }
+        Xlog.i("%d/%d posts filtered", adCount, postCount);
     }
 
     private static void blockAdProvider(XC_LoadPackage.LoadPackageParam lpparam) {
@@ -101,7 +109,27 @@ public class Hook implements IXposedHookLoadPackage {
             });
     }
 
-    private static void blockGraywaterTimelineAds(XC_LoadPackage.LoadPackageParam lpparam) {
+    private static void blockGraywaterTimelineAdsMulti(XC_LoadPackage.LoadPackageParam lpparam) {
+        Xutil.findAndHookMethod(
+            "com.tumblr.ui.widget.graywater.GraywaterTimelineAdapter", lpparam.classLoader,
+            "applyItems", List.class, boolean.class, boolean.class,
+            new XC_MethodHook() {
+                @Override
+                protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                    try {
+                        removeAds((List<?>)param.args[0]);
+                    } catch (Throwable e) {
+                        Xlog.e("Error occurred while filtering ads", e);
+                        throw e;
+                    }
+                }
+            });
+    }
+
+    private static void blockGraywaterTimelineAdsSingle(XC_LoadPackage.LoadPackageParam lpparam) {
+        // This one is for the ones that somehow fall through the
+        // cracks after filtering the timeline. Not sure if this
+        // is still necessary, but better safe than sorry, I guess.
         Xutil.findAndHookMethod(
             "com.tumblr.graywater.GraywaterAdapter", lpparam.classLoader,
             "add", int.class, Object.class, boolean.class,
@@ -109,7 +137,7 @@ public class Hook implements IXposedHookLoadPackage {
                 @Override
                 protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
                     try {
-                        if (isAd(param.args[1])) {
+                        if (isAd(param.args[1], false)) {
                             param.setResult(null);
                         }
                     } catch (Throwable e) {
@@ -136,10 +164,10 @@ public class Hook implements IXposedHookLoadPackage {
         Xlog.i("Tumblr AdAway initializing...");
         Xutil.printInitInfo(lpparam);
 
-        // forceEnableGraywater(lpparam);
         blockAdProvider(lpparam);
         blockSimpleTimelineAds(lpparam);
-        blockGraywaterTimelineAds(lpparam);
+        blockGraywaterTimelineAdsMulti(lpparam);
+        blockGraywaterTimelineAdsSingle(lpparam);
         blockExtendedFooter(lpparam);
 
         Xlog.i("Tumblr AdAway initialization complete!");
